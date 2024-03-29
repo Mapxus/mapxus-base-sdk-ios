f6cc01c27f44436f0386c37f98554fc3047b38b1
guochenghao <chenghaoguo@mapxus.com>
2024-03-29 18:36:56 +0800

    # CHANGELOG
    ## v6.7.0 (2024-03-29)
    
    ### 📝 Changes
    
    * Synchronous version.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "6.6.0"
+let version = "6.7.0"
 
 let package = Package(
   name: "MapxusBaseSDK",
@@ -20,7 +20,7 @@ let package = Package(
     .binaryTarget(
       name: "MapxusBaseSDK",
       url: "https://nexus3.mapxus.com/repository/ios-sdk/\(version)/mapxus-base-sdk-ios.zip",
-      checksum: "a4148bca69d40ac9255045bad020d8993228756574bb7b765ce5f75e088d025e"
+      checksum: "39415174d55eb4fd4db3708a1ca37042186034c7ea7bda7d833e9bdca6ca8a67"
     )
   ]
 )
